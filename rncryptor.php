370e06533bff0abc11286735ff1f6c6ead9405d9
Enrico Deleo <enrico@lysergicstudio.com>
2015-04-20 23:43:13 +0200

    ready

--- rncryptor.php
+++ rncryptor.php
@@ -12,3 +12,13 @@ License: MIT
 
 defined('ABSPATH') or die("No script kiddies please!");
 
+if ( file_exists( dirname( __FILE__ ) . '/vendor/RNCryptor-php/autoload.php' ) ) {
+  // Initialize after all plugins are available
+  add_action( 'plugins_loaded', 'rncryptor_init' );
+  function rncryptor_init() {
+    // Bootstrapping the RNCryptor library
+    require dirname( __FILE__ ).'/vendor/RNCryptor-php/autoload.php';
+  }
+}
+
+
